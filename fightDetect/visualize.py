353977038e2e04a66ed7340429c503b772b04dd1
Song-Gq <guanqunsong@qq.com>
2023-10-19 19:29:08 +0800

    bug fix in visualize

--- fightDetect/visualize.py
+++ fightDetect/visualize.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import plotly
 import pandas as pd
 
-src_name = "private/空手扭打1.json"
+src_name = "fight-sur/fi001.json"
 
 if __name__ == '__main__':
     df = pd.read_json('fightDetect/data/' + src_name)
@@ -13,8 +13,7 @@ if __name__ == '__main__':
     keys = pd.DataFrame(df.keypoints.tolist(), index=df.index)
     keys.columns = keys.columns.map(str)
     keys = pd.concat([df, keys], axis=1)
-    # delete keypoints 26-68
-    keys.drop(columns=keys.columns[[2, 4] + list(range(81, 210))], axis=1, inplace=True)
+    keys.drop(columns=keys.columns[[2, 4]], axis=1, inplace=True)
 
     # split the column of <list> 'box'
     boxes = pd.DataFrame(df.box.tolist(), index=df.index)
@@ -33,10 +32,10 @@ if __name__ == '__main__':
     # calculate the speed
     speed = keys.sort_values(by=['idx', 'image_id'])
     diff = speed.groupby('idx').diff().fillna(0.)
-    confi_cols = list(range(2, 75, 3))
+    confi_cols = list(range(2, 78, 3))
     for c in confi_cols:
         diff[str(c)] = np.sqrt(diff[str(c-1)]**2 + diff[str(c-2)]**2) / diff['image_id']
-    diff = diff[[str(c) for c in range(2, 75, 3)]]
+    diff = diff[[str(c) for c in range(2, 78, 3)]]
     diff = diff.add_suffix('_speed')
     speed = pd.concat([speed, diff], axis=1)
 
